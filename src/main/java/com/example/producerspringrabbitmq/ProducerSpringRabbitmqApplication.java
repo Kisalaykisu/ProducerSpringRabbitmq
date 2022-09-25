a3aaeadc57b8e8fcd788f9ffae192a184beee811
Kisalay <kisalaykisu@gmail.com>
2022-09-25 14:54:47 +0530

    first commit

---
+++ src/main/java/com/example/producerspringrabbitmq/ProducerSpringRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.producerspringrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerSpringRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerSpringRabbitmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/producerspringrabbitmq/ProducerSpringRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.producerspringrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerSpringRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
